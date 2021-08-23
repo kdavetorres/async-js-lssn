9859abeab56a5299299b0fadccbd08ded06c78e6
Azriel <aoirres@gmail.com>
2021-08-23 09:29:08 +0800

    Different readyState stages, Output json datavia responseText method

--- sandbox.js
+++ sandbox.js
@@ -5,6 +5,25 @@ request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); // API endpo
 request.send(); 
 */
 
+const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange', () => {
+ /*
+ 0 - UNSENT
+ 1 - OPENED
+ 2 - HEADERS_RECEIVED
+ 3 - lOADING
+ 4 - DONE
+ */
+ // console.log(request, request.readyState);
+ if(request.readyState === 4) {
+  console.log(request.responseText);
+ }
+});
+
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/'); // API endpoint
+request.send(); 
+
 
 
 
