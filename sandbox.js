33f60b76b6d6d9a52edf7e148c544140fa43c0b9
Azriel <aoirres@gmail.com>
2021-08-24 11:55:14 +0800

    Status code

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,7 @@
 const request  = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
- if(request.readyState === 4 && request.status === 2000) {
+ if(request.readyState === 4 && request.status === 200) {
   console.log(request, request.responseText);
  } else if(request.readyState === 4) {
   console.log('could not fetch the data');
